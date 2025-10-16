334ccdc75dc16e426e9bcf5fd240114d5159f039
Baenat <bae.julian10@gmail.com>
2025-10-16 16:24:14 -0500

    Implementa visualizacion de los productos en el componente homePage

--- src/app/products/components/product-card/product-card.component.ts
+++ src/app/products/components/product-card/product-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { SlicePipe } from '@angular/common';
+import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Product } from '@products/interfaces/product.interface';
+import { ProductImagePipe } from '@products/pipes/product-image.pipe';
 
 @Component({
   selector: 'product-card',
-  imports: [RouterLink],
+  imports: [RouterLink, SlicePipe, ProductImagePipe],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
-export class ProductCardComponent { }
+export class ProductCardComponent {
+  product = input.required<Product>();
+}
---
+++ src/app/products/pipes/product-image.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
+
+@Pipe({
+  name: 'productImage'
+})
+export class ProductImagePipe implements PipeTransform {
+
+  transform(value: string | string[]): string {
+    const noImage = `./assets/images/no-image.jpg`;
+
+    if (Array.isArray(value)) {
+      return value.length ? `${baseUrl}/files/product/${value[0]}` : noImage;
+    }
+
+    if (value && typeof value === 'string') {
+      return `${baseUrl}/files/product/${value}`;
+    }
+
+    return noImage;
+  }
+
+}
