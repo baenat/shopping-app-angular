7d8debd6af2ca81d879356e3485e940fe0dbb377
Baenat <bae.julian10@gmail.com>
2025-11-05 11:29:48 -0500

    Implementa componente de paginacion para la pagina de productos por genero

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -4,10 +4,12 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '@products/services/products.service';
 import { map } from 'rxjs';
 import { ProductCardComponent } from "@products/components/product-card/product-card.component";
+import { PaginationComponent } from "@shared/components/pagination/pagination.component";
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './gender-page.component.html',
   styleUrl: './gender-page.component.css',
 })
@@ -15,13 +17,20 @@ export class GenderPageComponent {
 
   _route = inject(ActivatedRoute);
   _productService = inject(ProductsService);
+  _paginationService = inject(PaginationService);
 
   gender = toSignal<string>(this._route.params.pipe(map((params) => params['gender'])));
 
   productsResource = rxResource({
-    request: () => (this.gender()),
+    request: () => ({
+      gender: this.gender(),
+      page: this._paginationService.currentPage() - 1
+    }),
     loader: ({ request }) => {
-      return this._productService.getProducts({ gender: request });
+      return this._productService.getProducts({
+        gender: request.gender,
+        offset: request.page * 9,
+      });
     }
   });
 
