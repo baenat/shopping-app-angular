58d5d65988f8934ed510223fb9a6818c15ec50ac
Baenat <bae.julian10@gmail.com>
2025-11-05 10:32:58 -0500

    Implementa listado de productos por genero

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
+import { ProductCardComponent } from "@products/components/product-card/product-card.component";
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './gender-page.component.html',
   styleUrl: './gender-page.component.css',
 })
-export class GenderPageComponent { }
+export class GenderPageComponent {
+
+  _route = inject(ActivatedRoute);
+  _productService = inject(ProductsService);
+
+  gender = toSignal<string>(this._route.params.pipe(map((params) => params['gender'])));
+
+  productsResource = rxResource({
+    request: () => (this.gender()),
+    loader: ({ request }) => {
+      return this._productService.getProducts({ gender: request });
+    }
+  });
+
+}
