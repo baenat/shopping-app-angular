d30cd86bf230e770b337613cf46deb6dc086bdd5
Baenat <bae.julian10@gmail.com>
2025-11-05 11:45:09 -0500

    Implementa funcionadlidad para guardar en cache los productos que fueron previamente consultados, mejorando experiencia de usuario

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { Product, ProductsResponse } from '@products/interfaces/product.interface';
 import { GeneralService } from '@shared/services/general.services';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 const baseUrl = environment.baseUrl;
@@ -18,11 +18,21 @@ interface Options {
 export class ProductsService {
 
   private _generalService = inject(GeneralService);
+  private productsCache = new Map<string, ProductsResponse>();
 
   getProducts(options: Options): Observable<ProductsResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
 
-    return this._generalService.get<ProductsResponse>(`${baseUrl}/products`, { params: { limit, offset, gender } }).pipe(tap(console.log));
+    const key = `${limit}-${offset}-${gender}`;
+
+    if (this.productsCache.has(key)) {
+      return of(this.productsCache.get(key)!);
+    }
+
+    return this._generalService.get<ProductsResponse>(`${baseUrl}/products`, { params: { limit, offset, gender } }).pipe(
+      tap(console.log),
+      tap((products) => this.productsCache.set(key, products))
+    );
   }
 
   getProductByIdSlug(slug: string): Observable<Product> {
