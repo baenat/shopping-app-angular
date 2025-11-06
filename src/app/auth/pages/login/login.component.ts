13fffd90369b037190946dfc708cc786a6a3f1a0
Baenat <bae.julian10@gmail.com>
2025-11-06 15:08:25 -0500

    Implementa formulario reactivo para autenticación del usuario

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginPageComponent {
+
+  _formBuilder = inject(FormBuilder);
+
+  hasError = signal(false);
+  isPosting = signal('');
+
+  loginForm = this._formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 3000);
+      return;
+    }
+
+    const { email = '', password = '' } = this.loginForm.value;
+
+    console.log({ email, password });
+  }
 
 }
