52d04b139abef301e8177478a1640a67ed9c01b9
Baenat <bae.julian10@gmail.com>
2025-11-13 15:54:52 -0500

    Añade pantalla de registro, formulario y validaciones

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterPageComponent {
+
+  _formBuilder = inject(FormBuilder);
+  _router = inject(Router);
+  _authService = inject(AuthService);
+
+  hasError = signal(false);
+
+  registerForm: FormGroup = this._formBuilder.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 3000);
+      return;
+    }
+
+    const { name = '', email = '', password = '' } = this.registerForm.value;
+
+    console.log(this.registerForm.value);
+  }
 
 }
