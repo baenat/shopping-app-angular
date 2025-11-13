b9f7a8bcd06b443cf928b7d1f82cb5d5e1cd6fcc
Baenat <bae.julian10@gmail.com>
2025-11-13 15:40:32 -0500

    Añade guard para verificar si el usuario tiene autenticación activa para redirigir a la pagina principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { notAuthenticatedGuard } from '@auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.routes').then(r => r.authRoutes)
+    loadChildren: () => import('./auth/auth.routes').then(r => r.authRoutes),
+    canMatch: [notAuthenticatedGuard]
   },
   {
     path: '',
