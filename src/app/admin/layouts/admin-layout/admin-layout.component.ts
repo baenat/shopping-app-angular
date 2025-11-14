04dd08b735fb0496be1025b2a6b8a0e7c896f877
Baenat <bae.julian10@gmail.com>
2025-11-14 18:50:36 -0500

    Diseño del panel administrativo

--- src/app/admin/layouts/admin-layout/admin-layout.component.ts
+++ src/app/admin/layouts/admin-layout/admin-layout.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css'
 })
 export class AdminLayoutComponent {
+
+  authService = inject(AuthService);
 
 }
