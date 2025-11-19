3b19b48769765d7721af41336c4d5847d1b12e10
Baenat <bae.julian10@gmail.com>
2025-11-19 18:25:38 -0500

    Consume servicio de productByID para obtener un producto por el ID

--- src/app/admin/pages/product-page-admin/product-page-admin.component.ts
+++ src/app/admin/pages/product-page-admin/product-page-admin.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-product-page-admin',
@@ -7,5 +11,22 @@ import { Component } from '@angular/core';
   styleUrl: './product-page-admin.component.css'
 })
 export class ProductPageAdminComponent {
+
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+  productsService = inject(ProductsService);
+
+  productId = toSignal(this.activatedRoute.params.pipe(map(params => params['id'])));
+
+  productResource = rxResource({
+    request: () => ({ id: this.productId() }),
+    loader: ({ request }) => this.productsService.getProductById(request.id)
+  });
+
+  redirectEffect = effect(() => {
+    if (this.productResource.error()) {
+      this.router.navigateByUrl('/admin/products');
+    }
+  });
 
 }
